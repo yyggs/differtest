891f5d6815e45632baa7a081e4deff46a53b4131
Chen Bo <1652496272@qq.com>
2024-06-10 19:27:50 +0100

    cow30

--- blocksize.sh
+++ blocksize.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# 指定输出文件名
 output_file="Level2.out"
 
-# 检查文件是否存在
 if [ ! -f "$output_file" ]; then
     echo "Error: Output file does not exist."
     exit 1
 fi
 
-# 使用awk读取文件中的压缩块大小并计算平均值
 awk '/Compressed block size:/ {sum += $4; count++} END {if (count > 0) print "Average Compressed Block Size: " sum / count " bytes"}' $output_file
